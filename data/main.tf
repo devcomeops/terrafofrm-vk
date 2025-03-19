58aa9f5d4d27eb35f3dbb9b480761bccf2db1b12
devcomeops <devcomeops@gmail.com>
2025-03-19 17:32:42 +0530

    push data- to fetch sg id from aws

--- data/main.tf
+++ data/main.tf
@@ -1,7 +1,12 @@
+provider "aws" {
+    region = "us-east-1"
+  
+}
+
 resource "aws_instance" "test" {
   ami                    = "ami-08b5b3a93ed654d19"
   instance_type          = "t2.micro"
-  vpc_security_group_ids = ["sg-012f6ef3ea01a686a"]
+  vpc_security_group_ids = ["data.aws_security_group.sd.id"]
 
   tags = {
     Name = "Test Server"
@@ -12,4 +17,8 @@ data "aws_ami" "ami" {
   most_recent      = true
   name_regex       = "RHEL-9-DevOps-Practice"
   owners           = ["973714476881"]
+}
+
+data "aws_security_group" "selected" {
+  name = "allow-all"
 }
